f51a9294c8207b1a59ad7565793c05e726be34cb
mystick minal <mystick14minal@gmail.com>
2024-11-20 21:52:26 +0545

    minal-base branch created

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
 
     return MaterialApp(
       title: 'Flutter Demo',
+
       theme: ThemeData(
         // This is the theme of your application.
         //
